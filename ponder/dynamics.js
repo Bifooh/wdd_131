ee5cb911e5b792160d7d9a986309b3e950aace2c
Samuel Pereira <75548737+Bifooh@users.noreply.github.com>
2026-05-23 23:57:51 -0600

    Add blog and dynamic content features
    - Created a new blog page (index.html) with a book review layout, including metadata and a book cover image.
    - Added corresponding styles (styles.css) for the blog page, enhancing layout and design with responsive features.
    - Introduced a dynamic content page (dynamic.html) for displaying a student list, with a linked JavaScript file (dynamics.js) to handle data rendering.
    - Implemented JavaScript functionality to dynamically generate and display student names on the dynamic content page.

---
+++ ponder/dynamics.js
@@ -0,0 +1,31 @@
+const student = [
+    {
+        lastName: "Perez",
+        firstName: "Anthony"
+    },
+    {
+        lastName: "Smith",
+        firstName: "John"
+    },
+    {
+        lastName: "Doe",
+        firstName: "Jane"
+    }
+];
+
+const container = document.querySelector("#student_container");
+
+student.forEach(function(item) {
+    let name = document.createElement("div");
+    name.calssName = "format";
+
+
+    let html = `        
+        <p class='details'>${item.firstName}</p>
+        <p class='details'>${item.lastName}</p>
+        <hr>
+        `
+
+    name.innerHTML = html;
+    container.appendChild(name);    
+})
